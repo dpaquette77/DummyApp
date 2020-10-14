e6ecc499aab5c7f8874ee6dfe1b11f0f41ee78ef
dpaquette <dpaquette@ubity.com>
2020-10-14 04:36:33 -0400

    use json to store application configuration params

--- DummyApp.go
+++ DummyApp.go
@@ -1,52 +1,76 @@
 package main
 
 import "fmt"
+import "io/ioutil"
 import "log"
 import "os"
+
+import "encoding/json"
 import "net/http"
 import "gopkg.in/ini.v1"
 
 var cfg *ini.File
 
+var appConfig DummyAppConfig
+var configFilePath = "/Users/dp/go/DummyApp/DummyApp.json"
+
+type DummyAppConfig struct {
+	ReadDbConfig  ReadMySqlConnectionConfig  `json:"read_db"`
+	WtiteDbConfig WriteMySqlConnectionConfig `json:"write_db"`
+	Logfile       string                     `json:"logfile"`
+	Port          int                        `json:"port"`
+}
+
+type ReadMySqlConnectionConfig struct {
+	Server   string `json:"server"`
+	Database string `json:"database"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type WriteMySqlConnectionConfig struct {
+	Server   string `json:"server"`
+	Database string `json:"database"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
-	var err error
-	cfg, err = ini.Load("DummyApp.ini")
+	jsonFile, err := os.Open(configFilePath)
 	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
+		fmt.Printf("Error while attempting to read config file: %w", err)
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+
+	// convert jsonFile to a byte array
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Printf("Error parsing config file: %w", err)
 		log.Fatal(err)
 	}
 
-	// Print configuration parameters to stdout
-	fmt.Println(cfg.Section("read_db").Key("server").String())
-	fmt.Println(cfg.Section("read_db").Key("database").String())
-	fmt.Println(cfg.Section("read_db").Key("username").String())
-	fmt.Println(cfg.Section("read_db").Key("password").String())
-
-	fmt.Println(cfg.Section("write_db").Key("server").String())
-	fmt.Println(cfg.Section("write_db").Key("database").String())
-	fmt.Println(cfg.Section("write_db").Key("username").String())
-	fmt.Println(cfg.Section("write_db").Key("password").String())
-
-	fmt.Println(cfg.Section("DummyApp").Key("logfile").String())
-	fmt.Println(cfg.Section("DummyApp").Key("response_prefix").String())
-	fmt.Println(cfg.Section("DummyApp").Key("port").String())
+	// Unmarshal json to appConfig struct
+	// TODO: add error checks
+	json.Unmarshal(jsonBytes, &appConfig)
 
 	// initialize the log file
-	file, err := os.OpenFile(cfg.Section("DummyApp").Key("logfile").String(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logFile, err := os.OpenFile(appConfig.Logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.SetOutput(file)
+	defer logFile.Close()
+	log.SetOutput(logFile)
 
 	// setup the http server stuff to handle requests to /
 	http.HandleFunc("/", DummyHttpServer)
 
-	log.Printf("attempting to listen on port %s", cfg.Section("DummyApp").Key("port").String())
-	http.ListenAndServe(":"+cfg.Section("DummyApp").Key("port").String(), nil)
+	log.Printf("attempting to listen on port %s", appConfig.Port)
+	http.ListenAndServe(fmt.Sprintf(":%d", appConfig.Port), nil)
 }
 
 func DummyHttpServer(w http.ResponseWriter, r *http.Request) {
-	response := fmt.Sprintf("%s request path: %s", cfg.Section("DummyApp").Key("response_prefix").String(), r.URL.Path[1:])
+	response := fmt.Sprintf("DummyApp request path: %s", r.URL.Path[1:])
 	log.Print(response)
 	fmt.Fprintf(w, response)
 }
